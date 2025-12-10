95e9a9549bfc889793b38d925dd8d0be378d2518
Luis Contreras <contreras.tellez.lm@outlook.es>
2025-12-10 11:13:55 -0600

    Probando modo cohete inestable

--- telemetria.py
+++ telemetria.py
@@ -1,2 +1,6 @@
 velocidad_actual = 340  # ¡Mejora de motor!
 print("La velocidad del MCL38 es:", velocidad_actual, "km/h")
+
+# CÓDIGO EXPERIMENTAL - NO TOCAR
+velocidad_actual = 9999
+print("Modo Cohete activado:", velocidad_actual)
